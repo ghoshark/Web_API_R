36bb79a19c70b9cb4a23c0e6b524c0c98ead36c8
ghoshark <arkaghosh.nb@gmail.com>
2018-08-15 11:05:53 +0530

    Initial commit

---
+++ SLA_API.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+rem --
+rem R Swagger API batch file
+rem --
+
+set R_HOME=C:/Program Files/R/R-3.5.0/bin/x64
+cd /D "%R_HOME%"
+
+Rscript.exe C:/Data_Science/SLAPrediction/R/server_sla.R 
